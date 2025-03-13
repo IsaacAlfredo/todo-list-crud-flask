f21e2472f0473bb5dcf0ec130ac6ee01a1d87f76
Isaac Alfredo <isaacfreitas23@gmail.com>
2025-03-13 17:57:32 -0300

    Todo model and DB creation

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
+from sqlalchemy.orm import DeclarativeBase, Mapped, MappedAsDataclass, mapped_column
 
 
 class Base(DeclarativeBase, MappedAsDataclass):
@@ -12,3 +12,14 @@ db = SQLAlchemy(model_class=Base)
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todoinfo.db"
 db.init_app(app)
+
+
+class Todo(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, init=False)
+    title: Mapped[str] = mapped_column(unique=True, nullable=False)
+    description: Mapped[str]
+    check: Mapped[bool] = mapped_column(default=False)
+
+
+with app.app_context():
+    db.create_all()
